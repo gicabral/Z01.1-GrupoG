bef84751db49026d083e3361671f8d6b6ad9315f
luizasilveira <luizars7@hotmail.com>
2019-03-07 18:35:25 -0300

    funcionando mux4way16

--- Projetos/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux4Way16.vhd
@@ -13,6 +13,11 @@ end entity;
 
 architecture arch of Mux4Way16 is
 begin
+	q <= (not sel(1) and not sel(0) and a) or 
+		 (not sel(1) and sel(0) and b) or 
+		 (sel(1) and not sel(0) and c) or 
+		 (sel(1) and sel(0) and d);
+
 
 
 end architecture;
