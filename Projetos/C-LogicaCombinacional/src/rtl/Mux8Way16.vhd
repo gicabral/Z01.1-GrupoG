b19d9c9a41ad238386f712f07242ba8494ef2ce8
borg <beatrizmkk@al.insper.edu.br>
2019-03-07 17:52:24 -0300

    funcionando Mux8Way16

--- Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
+++ Projetos/C-LogicaCombinacional/src/rtl/Mux8Way16.vhd
@@ -18,5 +18,13 @@ end entity;
 architecture arch of Mux8Way16 is
 begin
 
+	q<= (not sel(2) and not sel(1) and not sel(0) and a) or
+		(not sel(2) and not sel(1) and sel(0) and b) or
+		(not sel(2) and sel(1) and not sel(0) and c) or
+		(not sel(2) and sel(1) and sel(0) and d) or
+		(sel(2) and not sel(1) and not sel(0) and e) or
+		(sel(2) and not sel(1) and sel(0) and f) or
+		(sel(2) and sel(1) and not sel(0) and g) or
+		(sel(2) and sel(1) and sel(0) and h);
 
 end architecture;
