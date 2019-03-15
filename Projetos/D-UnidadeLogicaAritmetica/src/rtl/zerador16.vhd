01aa1941745e4d850b003066b59812e8b4c929df
gicabral <giovannasc@al.insper.edu.br>
2019-03-15 08:30:33 -0300

    funcionando o zerador16

--- Projetos/D-UnidadeLogicaAritmetica/src/rtl/zerador16.vhd
+++ Projetos/D-UnidadeLogicaAritmetica/src/rtl/zerador16.vhd
@@ -8,7 +8,7 @@ use IEEE.STD_LOGIC_1164.all;
 entity zerador16 is
   port(
         z   : in STD_LOGIC;
-	      a   : in STD_LOGIC_VECTOR(15 downto 0);
+	    a   : in STD_LOGIC_VECTOR(15 downto 0);
         y   : out STD_LOGIC_VECTOR(15 downto 0)
       );
 end zerador16;
@@ -18,7 +18,16 @@ architecture rtl of zerador16 is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
-begin
+begin 
+process (z,y)
+begin -- process
   -- Implementação vem aqui!
+  if (z = '1') then
+  	y <= (others => '0');
+  elsif (z = '0') then
+  	y <= a;
+  end if;		
+
+end process;
 
 end architecture;
