72ac5c9e52070e635adb37ed77bc077a0940d163
gicabral <giovannasc@al.insper.edu.br>
2019-03-20 14:55:03 -0300

    Funcionando FlipFlopD

--- Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/FlipFlopD.vhd
@@ -20,6 +20,21 @@ architecture arch of FlipFlopD is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
+begin process(clock, clear, preset)
 begin
+	if (preset = '1') then 
+		q <= '1';
+
+	elsif (clear = '1') then 
+		q<= '0';
+
+	elsif (rising_edge(clock)) then
+		q <= d;
+
+	end if;
+end process;
+
+
+
 
 end architecture;
