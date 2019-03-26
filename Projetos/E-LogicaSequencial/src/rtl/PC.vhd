1f8d1d1dabd87d4e2a04894d24cecd9d4200607e
mayrapeter <mayra11peter@gmail.com>
2019-03-26 19:21:29 -0300

    PC FUNCIONANDO COM O MUX E O REGISTER

--- Projetos/E-LogicaSequencial/src/rtl/PC.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/PC.vhd
@@ -1,14 +1,3 @@
--- Elementos de Sistemas
--- developed by Luciano Soares
--- file: PC.vhd
--- date: 4/4/2017
-
--- Contador de 16bits
--- if (reset[t] == 1) out[t+1] = 0
--- else if (load[t] == 1)  out[t+1] = in[t]
--- else if (inc[t] == 1) out[t+1] = out[t] + 1
--- else out[t+1] = out[t]
-
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.NUMERIC_STD.ALL;
@@ -20,7 +9,7 @@ entity PC is
         load      : in  STD_LOGIC;
         reset     : in  STD_LOGIC;
         input     : in  STD_LOGIC_VECTOR(15 downto 0);
-        output    : out STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000"
+        output    : out STD_LOGIC_VECTOR(15 downto 0)
     );
 end entity;
 
@@ -29,31 +18,35 @@ architecture arch of PC is
   -- e componentes (outros módulos) que serao
   -- utilizados nesse modulo.
 
-  component Inc16 is
+component inc16 is
   port(
     a   :  in STD_LOGIC_VECTOR(15 downto 0);
     q   : out STD_LOGIC_VECTOR(15 downto 0)
   );
   end component;
-SIGNAL saida, saida_antes : STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000";
 
+component Mux16 is
+  port ( 
+      a:   in  STD_LOGIC_VECTOR(15 downto 0);
+      b:   in  STD_LOGIC_VECTOR(15 downto 0);
+      sel: in  STD_LOGIC;
+      q:   out STD_LOGIC_VECTOR(15 downto 0));
+  end component;
+
+component Register16 is
+  port(
+    clock:   in STD_LOGIC;
+    input:   in STD_LOGIC_VECTOR(15 downto 0);
+    load:    in STD_LOGIC;
+    output: out STD_LOGIC_VECTOR(15 downto 0) := "0000000000000000"
+  );
+  end component;
+SIGNAL saida,q_mux0, q_mux1, q_mux2, saida_inc: std_logic_vector(15 downto 0) := "0000000000000000";
 begin
-
-  inc: Inc16 port map (saida, saida_antes);
+  inc : inc16 port map(saida, saida_inc);
+  mux0 : Mux16 port map(saida, saida_inc, increment, q_mux0);
+  mux1 : Mux16 port map(q_mux0, input,load, q_mux1);
+  mux2 : Mux16 port map(q_mux1, "0000000000000000", reset, q_mux2);
+  reg : Register16 port map(clock, q_mux2, '1', saida);
   output <= saida;
-
-  process(clock)
-    begin
-    if (rising_edge(clock)) then
-      if (reset = '1')  then
-        saida <= "0000000000000000";
-      elsif (load = '1') then
-        saida <= input;
-      elsif (increment = '1') then
-        saida <= saida_antes;
-      end if;
-    end if;
-  end process;
-
-
 end architecture;
