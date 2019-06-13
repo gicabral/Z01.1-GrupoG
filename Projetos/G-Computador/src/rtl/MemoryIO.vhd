bcac6f676b289852458396ba092db05c3bab5f36
mayrapeter <mayra11peter@gmail.com>
2019-06-13 10:05:36 -0300

    memory IO funfando

--- Projetos/G-Computador/src/rtl/MemoryIO.vhd
+++ Projetos/G-Computador/src/rtl/MemoryIO.vhd
@@ -29,10 +29,6 @@ entity MemoryIO is
      SW  : in std_logic_vector(9 downto 0);
      LED : OUT std_logic_vector(9 downto 0)
 
-     DADO : in STD_LOGIC_VECTOR(3 DOWNTO 0);
- 
-       -- Sinais de saída
-     A, B, C, D, E, F, G : out STD_LOGIC
 
     );
 end entity;
@@ -177,35 +173,7 @@ begin
     mux: Mux4Way16 port map (seletormux, sw_in2, mux_in1, mux_n1, mux_n2, OUTPUT );
 
 
-    with DADO select
-        S &lt;= "0000001" when "0000", -- '0'
-        "1001111" when "0001", -- '1'
-        "0010010" when "0010", -- '2'
-        "0000110" when "0011", -- '3'
-        "1001100" when "0100", -- '4'
-        "0100100" when "0101", -- '5'
-        "0100000" when "0110", -- '6'
-        "0001111" when "0111", -- '7'
-        "0000000" when "1000", -- '8'
-        "0000100" when "1001", -- '9'
-        "0001000" when "1010", -- 'A'
-        "1100000" when "1011", -- 'B'
-        "0110001" when "1100", -- 'C'
-        "1000010" when "1101", -- 'D'
-        "0110000" when "1110", -- 'E'
-        "0111000" when "1111", -- 'F'
-        "1111111" when others;
-
-    A &lt;= S(0);
-    B &lt;= S(1);
-    C &lt;= S(2);
-    D &lt;= S(3);
-    E &lt;= S(4);
-    F &lt;= S(5);
-    G &lt;= S(6);
-
   LED(9 downto 0) <= SaidaReg(9 downto 0);
-  sw_in2(9 downto 0) <= SW(9 downto 0);
-
+  
 
 END logic;
